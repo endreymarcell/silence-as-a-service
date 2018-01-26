84b05bfa639fb197b914fb63acc6d8076a4309ee
Marcell Endrey <marcell.endrey@prezi.com>
2018-01-26 17:33:43 +0100

    make it nicer

--- js/main.js
+++ js/main.js
@@ -1,70 +1,72 @@
-	function setup() {
-		createCanvas(windowWidth, windowHeight)
-	    mic = new p5.AudioIn()
-	    mic.start()
-	    dataPoints = []
-	    buffer = []
-	    lastPoint = 0
-	    UNIT_SIZE = 2
-	    UNIT_NUMBER = 1500
-		myVoice = new p5.Speech()
-		hasSpokenRecently = false
+LIMIT = 150
+WINDOW_SIZE = 200
+AVG_FACTOR = 5
 
-		THRESHOLD = 400
-		triggerCount = 0
+function setup() {
+	createCanvas(windowWidth, windowHeight)
+	myVoice = new p5.Speech()
+    mic = new p5.AudioIn()
+    mic.start()
+
+    dataPoints = []
+	avgPoints = []
+    maxWidth = width - 500
+    canSpeak = true
+}
+
+function draw() {
+	ambient_sound_level = mic.getLevel() * 1000
+	dataPoints.push(ambient_sound_level)
+	if (dataPoints.length > maxWidth) {
+		dataPoints = dataPoints.slice(dataPoints.length - maxWidth)
+	}
+	if (avgPoints.length > maxWidth) {
+		avgPoints = avgPoints.slice(avgPoints.length - maxWidth)
 	}
 
-	function draw() {
-		ambient_sound_level = mic.getLevel() * 1000
-		if (ambient_sound_level > THRESHOLD) {
-			triggerCount = max(triggerCount, 0)
-			triggerCount += 100
-		}
-		if (isTriggered() && !hasSpokenRecently) {
+	if (dataPoints.length >= WINDOW_SIZE) {
+		dataPointsInWindow = dataPoints.slice(dataPoints.length - WINDOW_SIZE)
+		avgPoint = dataPointsInWindow.reduce((a, b) => a + b, 0) / WINDOW_SIZE * AVG_FACTOR
+	} else {
+		avgPoint = 0
+	}
+	avgPoints.push(avgPoint)
+
+	background("white")
+	stroke(250, 20, 20, 100)
+	strokeWeight(1)
+	for (i = 0; i < dataPoints.length; i += 1) {
+		line(
+			i,
+			height,
+			i,
+			height - dataPoints[i]
+		)
+	}
+	stroke("darkred")
+	strokeWeight(3)
+	for (i = 0; i < avgPoints.length; i += 1) {
+		line(
+			i,
+			height - avgPoints[i - 1],
+			i,
+			height - avgPoints[i]
+		)
+	}
+	stroke("black")
+	strokeWeight(2)
+	line(0, height - LIMIT, width, height - LIMIT)
+
+	if (avgPoint > LIMIT) {
+		if (canSpeak) {
 			speak()
+			canSpeak = false
 		}
-	    buffer.push(Math.round(ambient_sound_level))
-	    if (buffer.length == UNIT_SIZE) {
-	    	dataPoint = buffer.reduce((a, b) => a + b, 0)
-	    	dataPoints.push(dataPoint)
-	    	buffer = []
-	    }
-	    if (dataPoints.length > UNIT_NUMBER) {
-	    	dataPoints = dataPoints.slice(dataPoints.length - UNIT_NUMBER)
-	    }
-	    background("white")
-	    stroke("green")
-	    strokeWeight(4)
-	    for (i = 0; i < UNIT_NUMBER; i += 1) {
-	    	if (dataPoints[i] && dataPoints[i - 1]) {
-		    	line(
-		    		(i - 1) * (width / UNIT_NUMBER),
-		    		getYForDataPoint(dataPoints[i - 1]),
-		    		(i) * (width / UNIT_NUMBER),
-		    		getYForDataPoint(dataPoints[i]),
-		    	)
-	    	}
-	    }
-	    stroke("red")
-	    strokeWeight(3)
-	    line(0, getYForDataPoint(THRESHOLD * 2), width, getYForDataPoint(THRESHOLD * 2))
-	    triggerCount -= 1
-	    if (triggerCount % 10 == 0) {
-	    	print(triggerCount)
-	    }
+	} else {
+		canSpeak = true
 	}
+}
 
-    function getYForDataPoint(dataPoint) {
-    	return height - dataPoint / 2
-    }
-
-    function speak() {
-    	myVoice.speak(random(messages))
-    	hasSpokenRecently = true
-    	setTimeout(function(){ hasSpokenRecently = false }, 3000)
-    	triggerCount = 0
-    }
-
-    function isTriggered() {
-    	return triggerCount > 600
-    }
+function speak() {
+	myVoice.speak(random(messages))
+}
